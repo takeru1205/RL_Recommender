6cb9d9777fc4166bf795d4a3f8f94180a5a8e08b
takeru1205 <takeru0639418@gmail.com>
2022-05-20 23:08:44 +0900

    [add] initial commit

--- src/network.py
+++ src/network.py
@@ -2,24 +2,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Net(nn.Module):
-    def __init__(self, state_dim, act_dim):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(state_dim, 32)
-        self.fc2 = nn.Linear(32, 32)
-        self.fc3 = nn.Linear(32, act_dim)
+class Policy(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Policy, self).__init__()
+        self.fc1 = nn.Linear(state_dim, 128)
+        self.dropout = nn.Dropout(p=0.6)
+        self.fc2 = nn.Linear(128, action_dim)
+
+        self.saved_log_probs = []
+        self.rewards = []
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        x = self.fc1(x)
+        x = self.dropout(x)
+        x = F.relu(x)
+        action_scores = self.fc2(x)
+        return F.softmax(action_scores, dim=1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import torch
+
     input_tensor = torch.rand(8, 16)
-    net = Net(state_dim=16, act_dim=4)
+    net = Policy(state_dim=16, action_dim=4)
     output_tensor = net(input_tensor)
     assert output_tensor.shape == (8, 4)
-
